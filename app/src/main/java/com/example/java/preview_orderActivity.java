18c65bc89e552038e3c9102041248f490a083065
Parmugilan S <mukilan895@gmail.com>
2024-06-21 00:15:00 +0530

    PDF UPLOADED to the firebase OPTION IN MAIN WITH PERMISSIONS and login and register activity..with the firebase of register authentication completed... history completed.user 1.

--- app/src/main/java/com/example/java/preview_orderActivity.java
+++ app/src/main/java/com/example/java/preview_orderActivity.java
@@ -1,78 +1,29 @@
 package com.example.java;
-import static java.security.AccessController.getContext;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ProgressDialog;
+
 
 import com.example.java.recyculer.PreviewAdapter;
-import com.github.barteksc.pdfviewer.util.FitPolicy;
-import android.content.DialogInterface;
-import android.content.Intent;
+
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.Editable;
+
 import android.text.InputFilter;
 import android.text.Spanned;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-import com.github.barteksc.pdfviewer.PDFView;
-import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ServerValue;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import android.view.View;
+import android.widget.ImageButton;
+
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class preview_orderActivity extends AppCompatActivity {
-    /*private TextView fileNameTextView, pg, amt1, finalamt, qtyno, qtytxt1, perpageamt, deliveryamt, colortxt;
-    private StorageReference storageRef;
-    private EditText qty;
-    private List<Uri> pdfUris;
-    private List<String> pdfNames;
-    private int currentPdfIndex = 0;
-    private String orderid;
-    private Spinner spinner, spinner1, spinner3;
     private ImageButton backbtn;
-    private int count = 1;
-    private float perpage = 0.75f;
-    private DatabaseReference databaseReference;
-    private CircleImageView black, gradient;
-    private ProgressDialog progressDialog;
-    private Button btn, preview;
-    private PDFView pdfView;
-    private String formats = "Front & Back", ratios = "1:1", sheet = "A4", Color = "Black";
-    private int pgsam;
-    private ImageButton plus, minus;
-    private Uri pdf;
-    private boolean isUploading = false;
-    private float currentProgress = 0, finalamount;
-    private float delivercharge = 10.0f, amtperqty;*/
 
     private RecyclerView recyclerView;
     private PreviewAdapter adapter;
@@ -81,30 +32,7 @@ public class preview_orderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_order);
-        /*spinner = findViewById(R.id.spinneradmin);
-        backbtn = findViewById(R.id.backadmin);
-        qtytxt1 = findViewById(R.id.qtytxt1admin);
-        qtyno = findViewById(R.id.qtynoadmin);
-        fileNameTextView = findViewById(R.id.filenametxt1admin);
-        black = findViewById(R.id.blackcolor);
-        gradient = findViewById(R.id.gradientcolor);
-
-        pg = findViewById(R.id.pagenoadmin);
-        spinner3 = findViewById(R.id.spinner3admin);
-        amt1 = findViewById(R.id.amt1admin);
-        spinner1 = findViewById(R.id.spinner1admin);
-        finalamt = findViewById(R.id.finalamtadmin);
-        qty = findViewById(R.id.qtytxtadmin);
-        preview = findViewById(R.id.preview);
-        btn = findViewById(R.id.orderbtnadmin);
-        perpageamt = findViewById(R.id.perpageamtadmin);
-        plus = findViewById(R.id.addqtyadmin);
-        qtyno = findViewById(R.id.qtynoadmin);
-        deliveryamt = findViewById(R.id.deliveryamt1admin);
-        minus = findViewById(R.id.minusqtyadmin);
-        pdfView = findViewById(R.id.pdfViewadmin);
-        qtyno.setText(String.valueOf(count));
-        colortxt = findViewById(R.id.colorfontadmin);*/
+        backbtn=findViewById(R.id.backadmin);
 
         recyclerView = findViewById(R.id.recyclerpreviewactivity);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -122,9 +50,21 @@ public class preview_orderActivity extends AppCompatActivity {
             Toast.makeText(this, "Failed to load files.", Toast.LENGTH_SHORT).show();
         }
 
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showExitConfirmationDialog();
+            }
+        });
+
 
 
     }
+
+    private void showExitConfirmationDialog() {
+
+    }
+
     class InputFilterMinMax implements InputFilter {
         private int min, max;
 
--- app/src/main/java/com/example/java/recyculer/PreviewAdapter.java
+++ app/src/main/java/com/example/java/recyculer/PreviewAdapter.java
@@ -1,20 +1,33 @@
 package com.example.java.recyculer;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.java.R;
 import com.github.barteksc.pdfviewer.PDFView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PreviewAdapter extends RecyclerView.Adapter<PreviewAdapter.ViewHolder> {
 
@@ -53,10 +66,145 @@ public class PreviewAdapter extends RecyclerView.Adapter<PreviewAdapter.ViewHold
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView fileNameTextView;
         private PDFView pdfView;
+        private TextView  pg, amt1, finalamt, qtyno, qtytxt1, perpageamt, deliveryamt, colortxt;
+        private StorageReference storageRef;
+        private EditText qty;
+        private int currentPdfIndex = 0;
+        private String orderid;
+        private Spinner spinner, spinner1, spinner3;
+
+        private int count = 1;
+        private float perpage = 0.75f;
+        private DatabaseReference databaseReference;
+        private CircleImageView black, gradient;
+        private ProgressDialog progressDialog;
+        private Button btn, preview;
+        private String formats = "Front & Back", ratios = "1:1", sheet = "A4", Color = "Black";
+        private int pgsam;
+        private ImageButton plus, minus;
+        private Uri pdf;
+        private boolean isUploading = false;
+        private float currentProgress = 0, finalamount;
+        private float delivercharge = 10.0f, amtperqty;
+
+
+
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             fileNameTextView = itemView.findViewById(R.id.filenametxt1admin);
             pdfView = itemView.findViewById(R.id.pdfViewadmin);
+            qtytxt1 = itemView.findViewById(R.id.qtytxt1admin);
+            qtyno = itemView.findViewById(R.id.qtynoadmin);
+            fileNameTextView = itemView.findViewById(R.id.filenametxt1admin);
+            black = itemView.findViewById(R.id.blackcolor);
+            gradient = itemView.findViewById(R.id.gradientcolor);
+            pg = itemView.findViewById(R.id.pagenoadmin);
+            spinner3 = itemView.findViewById(R.id.spinner3admin);
+            amt1 = itemView.findViewById(R.id.amt1admin);
+            spinner1 = itemView.findViewById(R.id.spinner1admin);
+            finalamt = itemView.findViewById(R.id.finalamtadmin);
+            qty = itemView.findViewById(R.id.qtytxtadmin);
+            preview = itemView.findViewById(R.id.preview);
+            btn = itemView.findViewById(R.id.orderbtnadmin);
+            perpageamt = itemView.findViewById(R.id.perpageamtadmin);
+            plus = itemView.findViewById(R.id.addqtyadmin);
+            qtyno = itemView.findViewById(R.id.qtynoadmin);
+            deliveryamt = itemView.findViewById(R.id.deliveryamt1admin);
+            minus = itemView.findViewById(R.id.minusqtyadmin);
+            pdfView = itemView.findViewById(R.id.pdfViewadmin);
+            colortxt = itemView.findViewById(R.id.colorfontadmin);
+
+            black.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    colortxt.setText("Black");
+                    Color="Black";
+                    updateamt();
+                }
+            });
+
+            gradient.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    colortxt.setText("Gradient");
+                    Color="Gradient";
+                    updateamt();
+                }
+            });
+
+            ArrayList<String> sheets=new ArrayList<>();
+            sheets.add("A4");
+            sheets.add("OHP");
+
+            ArrayAdapter<String> adapter3=new ArrayAdapter<>(context,R.layout.spinner_item_layout,sheets);
+            spinner3.setAdapter(adapter3);
+
+            spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    sheet=parent.getItemAtPosition(position).toString();
+                    updateamt();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+
+            ArrayList<String> format = new ArrayList<>();
+            format.add("Front & Back");
+            format.add("Front Only");
+            ArrayAdapter<String> Adaptor1 = new ArrayAdapter<>(
+                    context,
+                    R.layout.spinner_item_layout,
+                    format
+            );
+            Adaptor1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(Adaptor1);
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    formats = parent.getItemAtPosition(position).toString();
+                    updateamt();
+                    Toast.makeText(context, formats, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+
+            ArrayList<String> ratio = new ArrayList<>();
+            ratio.add("1:1");
+            ratio.add("1:2");
+            ratio.add("1:4");
+            ArrayAdapter<String> Adaptor2 = new ArrayAdapter<>(
+                    context,
+                    R.layout.spinner_item_layout,
+                    ratio
+            );
+            Adaptor2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner1.setAdapter(Adaptor2);
+            spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    ratios=parent.getItemAtPosition(position).toString();
+                    updateamt();
+                    Toast.makeText(context, ratios, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
+        private void updateamt() {
+
         }
 
         public void bind(Uri uri, String fileName) {
