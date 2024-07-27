d4b9fdaad94e9639f8a507d5cee9c301a3518834
Parmugilan S <mukilan895@gmail.com>
2024-07-27 15:35:02 +0530

    PDF UPLOADED to the firebase OPTION IN MAIN WITH PERMISSIONS and login and register activity..with the firebase of register authentication completed... history completed.user 1.v

--- app/src/main/java/com/example/java/history_fragment.java
+++ app/src/main/java/com/example/java/history_fragment.java
@@ -1,10 +1,13 @@
 package com.example.java;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,9 +27,11 @@ import java.util.List;
 
 public class history_fragment extends Fragment {
     private ViewPager2 bannerViewPager;
+    private LinearLayout pen,tippencil,drafter,aenote,calculator,graph;
     private RecyclerView recyclerView;
     private ProductlistAdaptor adaptor;
     private ArrayList<ProductDetails> productDetails;
+    private ImageButton cart;
 
     public history_fragment() {
     }
@@ -39,6 +44,67 @@ public class history_fragment extends Fragment {
         bannerViewPager = view.findViewById(R.id.bannerViewPager);
         recyclerView=view.findViewById(R.id.productrecyculer);
         productDetails=new ArrayList<>();
+        pen=view.findViewById(R.id.onclickpen);
+        tippencil=view.findViewById(R.id.onclicktippencil);
+        graph=view.findViewById(R.id.onclickgraph);
+        aenote=view.findViewById(R.id.onclicka3note);
+        drafter=view.findViewById(R.id.onclickdrafter);
+        calculator=view.findViewById(R.id.onclickcalculator);
+        cart=view.findViewById(R.id.mycarthome);
+
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), Mycart.class));
+            }
+        });
+
+        pen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),Productpreviewa.class));
+            }
+        });
+
+        graph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),Productpreviewa.class));
+
+            }
+        });
+
+        calculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),Productpreviewa.class));
+
+            }
+        });
+
+        aenote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),Productpreviewa.class));
+
+            }
+        });
+
+        drafter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),Productpreviewa.class));
+
+            }
+        });
+
+        tippencil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),Productpreviewa.class));
+
+            }
+        });
 
         productDetails.add(new ProductDetails("Calculator","750",R.drawable.calculator));
         productDetails.add(new ProductDetails("Graph","750",R.drawable.geaph));
