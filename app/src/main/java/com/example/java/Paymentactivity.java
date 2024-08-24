984df29f709c7ab237ec143f65045577733c5c9e
Parmugilan S <mukilan895@gmail.com>
2024-08-24 08:14:29 +0530

    PDF UPLOADED to the firebase OPTION IN MAIN WITH PERMISSIONS and login and register activity..with the firebase of register authentication completed... history completed.user 1.v

--- app/src/main/java/com/example/java/Paymentactivity.java
+++ app/src/main/java/com/example/java/Paymentactivity.java
@@ -1,5 +1,6 @@
 package com.example.java;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,9 +8,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import javax.annotation.Nullable;
+
 public class Paymentactivity extends AppCompatActivity {
     private TextView textView;
     private Orderconfirmuseractivity orderconfirmuseractivity;
+    private final static int UPI_PAYMENT_REQUEST_CODE=1;
     private Button btn;
 
 
@@ -29,6 +33,42 @@ public class Paymentactivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == UPI_PAYMENT_REQUEST_CODE) {
+            if (data != null) {
+                String response = data.getStringExtra("response");
+                if (response == null) response = "discard";
+
+                String status = "";
+                String approvalRefNo = "";
+                String[] responseArray = response.split("&");
+                for (String res : responseArray) {
+                    String[] equalStr = res.split("=");
+                    if (equalStr.length >= 2) {
+                        if (equalStr[0].equalsIgnoreCase("Status")) {
+                            status = equalStr[1].toLowerCase();
+                        } else if (equalStr[0].equalsIgnoreCase("ApprovalRefNo") || equalStr[0].equalsIgnoreCase("txnRef")) {
+                            approvalRefNo = equalStr[1];
+                        }
+                    }
+                }
+
+                if (status.equals("success")) {
+                    // Payment was successful, open SuccessAnimation activity
+                    Intent intent = new Intent(Paymentactivity.this, suceesanimation.class);
+                    startActivity(intent);
+                } else {
+                    // Payment failed or was canceled, handle accordingly
+                    //showError("Transaction failed or was canceled.");
+                }
+            } else {
+                //showError("Transaction failed or was canceled.");
+            }
+        }
     }
 
 }
