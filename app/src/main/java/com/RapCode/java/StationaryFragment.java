0cebbfe1cc257ac358363b09bb1f1ec5130da289
Parmugilan S <mukilan895@gmail.com>
2024-11-05 00:51:55 +0530

    PDF UPLOADED to the firebase OPTION IN MAIN WITH PERMISSIONS and login and register activity..with the firebase of register authentication completed... history completed.user 1.v

--- app/src/main/java/com/RapCode/java/StationaryFragment.java
+++ app/src/main/java/com/RapCode/java/StationaryFragment.java
@@ -98,10 +98,10 @@ public class StationaryFragment extends Fragment implements BannerAdapter.OnBann
     }
 
     private void setupCategoryListeners() {
-        graph.setOnClickListener(v -> openProductPreview("GRAPH NOTE BOOK - Practice Map 100 PAGES - A4 SIZE", "120", R.drawable.graphh));
+        graph.setOnClickListener(v -> openProductPreview("GRAPH NOTE BOOK - Practice Map 100 PAGES - A4 SIZE", "100", R.drawable.graphh));
         calculator.setOnClickListener(v -> openProductPreview("Caltrix CX-991S Scientific Calculator", "600", R.drawable.caltrix, "Brand\tCaltrix\nCalculator Type\tEngineering/Scientific\nPower Source\tBattery Powered\nModel Name\tCX 991S\nMaterial\tPlastic"));
-        aenote.setOnClickListener(v -> openProductPreview("A3 Sketch Book/Art Book/Drawing Book", "300", R.drawable.athreenotee));
-        drafter.setOnClickListener(v -> openProductPreview("ORFORX Mini Drafter with Heavy Mild Steel Rod & Shatterproof Scale for Engineering Student With Protective Cover (Blue)", "800", R.drawable.drafter1));
+        aenote.setOnClickListener(v -> openProductPreview("A3 Sketch Book/Art Book/Drawing Book", "80", R.drawable.athreenotee));
+        drafter.setOnClickListener(v -> openProductPreview("ORFORX Mini Drafter with Heavy Mild Steel Rod & Shatterproof Scale for Engineering Student With Protective Cover (Blue)", "350", R.drawable.drafter1));
         tippencil.setOnClickListener(v -> openProductPreview("Mechanical Pencils with 1 Tube Lead, 0.7mm Tip", "15", R.drawable.tippencil, "Name : Faber-Castell Tri-Click Mechanical Pencils with 1 Tube Lead, 0.7mm Tip\nBrand : Faber Castell\nNet Quantity (N) : 1\nErgonomic triangular grip zone\nRetractable metal sleeve-pocket safe\nTip size : 0.7mm\nSet includes mechanical pencil with one tube of lead (0.7mm)"));
     }
 
@@ -143,7 +143,7 @@ public class StationaryFragment extends Fragment implements BannerAdapter.OnBann
             productDetails.add(new ProductDetails("Stylish X3 Ball Pen - Black (0.7mm)","10",R.drawable.stylishblackpen));
             productDetails.add(new ProductDetails("Stylish X3 Ball Pen - Blue (0.7mm)","7",R.drawable.stylishpenblue));
             productDetails.add(new ProductDetails("Stylish X3 Ball Pen - Black (0.7mm)","7",R.drawable.stylishblackpen));
-            productDetails.add(new ProductDetails("A3 Sketch Book/Art Book/Drawing Book(40 pages)","60",R.drawable.athreenotee));
+            productDetails.add(new ProductDetails("A3 Sketch Book/Art Book/Drawing Book(40 pages)","80",R.drawable.athreenotee));
             productDetails.add(new ProductDetails("Mechanical Pencils with 1 Tube Lead, 0.7mm Tip","15",R.drawable.tippencil,1,"Name : Faber-Castell Tri-Click Mechanical Pencils with 1 Tube Lead, 0.7mm Tip\n" +
                     "\n" +
                     "Brand : Faber Castell\n" +
@@ -157,13 +157,13 @@ public class StationaryFragment extends Fragment implements BannerAdapter.OnBann
                     "Tip size : 0.7mm\n" +
                     "\n" +
                     "Set includes mechanical pencil with one tube of lead (0.7mm)"));
-            productDetails.add(new ProductDetails("Bipolar Graph (100 sheets)","140",R.drawable.bipolar,1));
+            productDetails.add(new ProductDetails("Bipolar Graph (100 sheets)","100",R.drawable.bipolar,1));
             productDetails.add(new ProductDetails("Tip Box Camlin Kokuyo 0.7mm Klick Lead Tube","5",R.drawable.tipbox,1,"Leads are tough, smooth and dark\n" +
                     "0.7mm B leads\n" +
                     "High polymer fine leads for smooth writing"));
             productDetails.add(new ProductDetails("Kokuyo Camlin Exam Scale Broad 30cm Ruler ","10",R.drawable.scale,1,"Camlin Exam portfolio of scales are the perfect tools for high precision and accuracy. Smooth taped edges that lie flat on the surface to give a sharp line. Transparent body"));
             productDetails.add(new ProductDetails("White Apsara Eraser","5",R.drawable.eraser,1,"As a quality focused firm, we are engaged in offering a high quality range of Apsara Eraser."));
-            productDetails.add(new ProductDetails("ORFORX Mini Drafter with Heavy Mild Steel Rod & Shatterproof Scale for Engineering Student With Protective Cover (Blue)","330",R.drawable.drafter1));
+            productDetails.add(new ProductDetails("ORFORX Mini Drafter with Heavy Mild Steel Rod & Shatterproof Scale for Engineering Student With Protective Cover (Blue)","350",R.drawable.drafter1));
             productDetails.add(new ProductDetails("A4 Sheet Bundle COPIER PAPER 80 GSM 500 SHEETS","280",R.drawable.afoursheet,1,"Brand\tTNPL\n" +
                     "Colour\tWhite\n" +
                     "Item Weight\t80 Grams\n" +
